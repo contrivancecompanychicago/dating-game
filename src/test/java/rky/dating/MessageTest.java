38032a74527b244e82decf5abd381cf270bf2243
rshepherd <me@rshepherd.org>
2012-11-16 02:03:53 -0500

    Fixing createCandidate test since order cannot be assumed in HashMap

--- src/test/java/rky/dating/MessageTest.java
+++ src/test/java/rky/dating/MessageTest.java
@@ -1,7 +1,7 @@
     package rky.dating;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
