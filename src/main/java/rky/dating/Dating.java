8b6f9e936832944a89e1eab9101b1cddafbb5079
rshepherd <me@rshepherd.org>
2012-11-15 22:42:21 -0500

    Initial check-in

---
+++ src/main/java/rky/dating/Dating.java
@@ -0,0 +1,11 @@
+package rky.dating;
+
+public class Dating
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Oh yeah!");
+    }
+
+}
---
+++ src/test/java/rky/dating/DatingTest.java
@@ -0,0 +1,17 @@
+package rky.dating;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DatingTest
+{
+
+    @Test
+    public void test()
+    {
+        assertTrue(true);
+        Dating.main(null);
+    }
+
+}
