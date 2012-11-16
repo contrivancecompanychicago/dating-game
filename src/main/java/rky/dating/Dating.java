040823c1969b9b8a58c5774828fd3c5f2733f185
stokedover9k <stokedover9k@gmail.com>
2012-11-16 17:45:35 -0500

    wrappers for vectors added and game loop demo in Dating.java

--- src/main/java/rky/dating/Dating.java
+++ src/main/java/rky/dating/Dating.java
@@ -1,11 +1,141 @@
 package rky.dating;
 
+import rky.dating.player.Player;
+import rky.dating.player.Player.Role;
+import rky.dating.primitives.Candidate;
+import rky.dating.primitives.Noise;
+import rky.dating.primitives.Preferences;
+
 public class Dating
 {
 
     public static void main(String[] args)
     {
-        System.out.println("Oh yeah!");
+    	// TODO: N should be a command line argument
+        int N = 40;
+        
+        runGame(N);
     }
+
+	private static void runGame(int n)
+	{
+		informPlayer( Player.Role.P, "P " + n );
+		informPlayer( Player.Role.M, "M " + n );
+		
+
+		//------------------------------------------------------
+		startTimer( Player.Role.P );
+		informPlayer( Player.Role.P, "WEIGHTS" );
+		
+		Preferences p = getPlayerPreferences(n);
+		pauseTimer( Player.Role.P );
+		
+		if( !p.isValid() ) {
+			informPlayerOfError( Player.Role.P, p.getCachedMsg() );
+		} else {
+			confirmPlayer( Player.Role.P, "accepted P");
+		}
+		//------------------------------------------------------
+		
+		
+		//------------------------------------------------------
+		StringBuilder randomCandidatesMsg = new StringBuilder();
+		for( int i = 0; i < 20; i++ )
+		{
+			Candidate c = Candidate.generateRandomCandidate( n );
+			randomCandidatesMsg.append( c.toString() );
+			double score = c.getScore( p );
+			randomCandidatesMsg.append( " " + score + " " );
+		}
+		randomCandidatesMsg.setLength( randomCandidatesMsg.length()-1 );
+		informPlayer( Player.Role.M, randomCandidatesMsg.toString() );
+		//------------------------------------------------------
+		
+		
+		//------------------------------------------------------
+		double maxScore = Double.NEGATIVE_INFINITY;
+		int turnNumber = 0;
+		for( ; turnNumber < 20; turnNumber++ )
+		{
+			if( maxScore == 1 )
+				break;
+
+			startTimer( Player.Role.M );
+			informPlayer( Player.Role.M, "CANDIDATE" );
+			// TODO receive candidate from user
+			Candidate c = getCandidateFromM( n );
+			pauseTimer( Player.Role.M );
+			if( c.isValid() )
+				confirmPlayer( Player.Role.M, "accepted Candidate" );
+			else
+				informPlayerOfError( Player.Role.M, c.getCachedMsg() );
+			
+			
+			
+			startTimer( Player.Role.P );
+			informPlayer( Player.Role.P, "NOISE" );
+			Noise noise = getNoiseFromM( p );  //TODO shouldn't need p, should already have it
+			pauseTimer( Player.Role.P );
+			if( noise.isValid( p ) )
+				confirmPlayer( Player.Role.P, "accepted Noise" );
+			else
+				informPlayerOfError( Player.Role.P, noise.getCachedMsg() );
+			
+			
+			
+			double score = c.getScore( p, noise );              // noisy score
+			informPlayer( Player.Role.M, "" + score );
+			maxScore = Math.max( maxScore, c.getScore( p ) );   // actual score
+			
+		}
+		
+		
+		
+		informPlayer( Player.Role.M, maxScore + " " + turnNumber );
+		informPlayer( Player.Role.P, maxScore + " " + turnNumber );
+	}
+
+	private static Noise getNoiseFromM(Preferences p) {
+		Noise n = Noise.generateRandomNoise( p );
+		System.out.println( "P->S: " + n );
+		return n;
+	}
+
+	private static Candidate getCandidateFromM(int n) {
+		Candidate c = Candidate.generateRandomCandidate( n ); 
+		System.out.println( "M->S: " + c );
+		return c;
+	}
+
+	private static void pauseTimer(Role m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void startTimer(Role m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void informPlayer( Player.Role role, String msg) {
+		// TODO Auto-generated method stub
+		System.out.println( "S->" + (role == Player.Role.M ? "M" : "P") + ": " + msg );
+	}
+
+	private static void confirmPlayer( Player.Role role, String clarification) {
+		// TODO Auto-generated method stub
+		System.out.println( "S->" + (role == Player.Role.M ? "M" : "P") + ": OK    # " + clarification );
+	}
+
+	private static void informPlayerOfError( Player.Role role, String errMsg) {
+		// TODO Actually send to players
+		System.out.println( "S->" + (role == Player.Role.M ? "M" : "P") + ": ERROR " + errMsg );
+	}
+
+	private static Preferences getPlayerPreferences(int n)
+	{
+		// TODO Actually get from player
+		return Preferences.generateRandomPreferences(n);
+	}
 
 }
---
+++ src/main/java/rky/dating/primitives/Preferences.java
@@ -0,0 +1,80 @@
+package rky.dating.primitives;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import vecs.Veci;
+
+public class Preferences extends Veci
+{
+	private String msgCache_ = null;
+	
+	public Preferences(int degree)
+	{
+		super(degree);
+	}
+	
+	
+	
+	public Preferences(ArrayList<Integer> prefsList)
+	{
+		super( prefsList );
+	}
+
+
+
+	// if preference is not valid, the reason may be retrieved via the getCachedMsg() method
+	public boolean isValid()
+	{
+		msgCache_ = validate();
+		return msgCache_ == null;
+	}
+	
+	// returns null if valid, error message otherwise
+	public String validate()
+	{
+		int positive = 0;
+		int negative = 0;
+		
+		for( int i = 0; i < getDegree(); i++ )
+		{
+			int val = get(i);
+			if( val > 0 )
+				positive += val;
+			else
+				negative += val;
+		}
+		
+		if( positive !=  100 )  return "positive preferences add up to " + positive;
+		if( negative != -100 )  return "negative preferences add up to " + negative;
+		
+		return null;
+	}
+	
+	public static Preferences generateRandomPreferences( int degree )
+	{
+		ArrayList<Integer> prefsList = new ArrayList<Integer>(degree);
+		for( int i = 0; i < degree; i ++ )
+			prefsList.add(0);
+		
+		// some of these will be 0's
+		int howManyPositives = 1 + (int)(Math.random() * (degree-1)) % (degree-1);  // must always be at least 1
+		int howManyNegatives = degree - howManyPositives;
+
+		for( int i = 0; i < 100; i++ ) {
+			int posIndex = (int)( Math.random() * howManyPositives ) % howManyPositives;
+			int negIndex = (int)(( Math.random() * howManyNegatives ) % howManyNegatives) + howManyPositives; 
+			prefsList.set( posIndex, prefsList.get(posIndex) + 1 );
+			prefsList.set( negIndex, prefsList.get(negIndex) - 1 );
+		}
+		
+		Collections.shuffle(prefsList);
+		
+		return new Preferences( prefsList );
+	}
+	
+	public String getCachedMsg()
+	{
+		return msgCache_;
+	}
+}
