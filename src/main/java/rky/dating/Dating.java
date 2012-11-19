3bb98f6c8468bb1141be512f7648ec3dd0d40fa2
stokedover9k <stokedover9k@gmail.com>
2012-11-19 01:02:06 -0500

    Player errors now terminate the game

--- src/main/java/rky/dating/Dating.java
+++ src/main/java/rky/dating/Dating.java
@@ -45,101 +45,112 @@ public class Dating
 
 	public static void runGame(int n)
 	{
-		//initaliztion is done by IOManager
-		//informPlayer( Player.Role.P, "P " + n ); 
-		//informPlayer( Player.Role.M, "M " + n );
-
-
-		//------------------------------------------------------
-		startTimer( Role.P );
-		Preferences p = getPlayerPreferences(n);
-		pauseTimer( Role.P );
-
-		if( !p.isValid() ) {
-			informPlayerOfError( Role.P, p.getCachedMsg() );
-		} else {
-			confirmPlayer( Role.P, "accepted P");
-		}
-		//------------------------------------------------------
-
-		//------------------------------------------------------
-		StringBuilder randomCandidatesMsg = new StringBuilder();
-		for( int i = 0; i < 20; i++ )
-		{
-			Candidate c = Candidate.generateRandomCandidate( n );
-			if( !c.isValid() )
-				throw new RuntimeException( "generated an illegal candidate: " + c.toString() + " -> " + c.getCachedMsg() );
-			randomCandidatesMsg.append( c.toString() );
-			double score = c.getScore( p );
-			randomCandidatesMsg.append( " " + String.format("%.2f", score) + " " );
-		}
-		randomCandidatesMsg.setLength( randomCandidatesMsg.length()-1 );
-		informPlayer( Role.M, randomCandidatesMsg.toString() );
-		//------------------------------------------------------
-
-
-		//------------------------------------------------------
-		double maxScore = Double.NEGATIVE_INFINITY;
+		String finalScore = "if this is showing...";
+		String turnsUsed = "something went horribly wrong";
+		
 		int turnNumber = 0;
-		for( ; turnNumber < 20; turnNumber++ )
+		double maxScore = Double.NEGATIVE_INFINITY;
+		
+		try {
+		//------------------------------------------------------
+			startTimer( Role.P );
+			Preferences p = getPlayerPreferences(n);
+			pauseTimer( Role.P );
+	
+			if( !p.isValid() ) {
+//				informPlayerOfError( Role.P, p.getCachedMsg() );
+				throw new PlayerErrorException( getPlayer(Role.P), p.getCachedMsg() );
+			} else {
+				confirmPlayer( Role.P, "accepted P");
+			}
+			//------------------------------------------------------
+	
+			//------------------------------------------------------
+			StringBuilder randomCandidatesMsg = new StringBuilder();
+			for( int i = 0; i < 20; i++ )
+			{
+				Candidate c = Candidate.generateRandomCandidate( n );
+				if( !c.isValid() )
+					throw new RuntimeException( "generated an illegal candidate: " + c.toString() + " -> " + c.getCachedMsg() );
+				randomCandidatesMsg.append( c.toString() );
+				double score = c.getScore( p );
+				randomCandidatesMsg.append( " " + String.format("%.2f", score) + " " );
+			}
+			randomCandidatesMsg.setLength( randomCandidatesMsg.length()-1 );
+			informPlayer( Role.M, randomCandidatesMsg.toString() );
+			//------------------------------------------------------
+	
+	
+			//------------------------------------------------------
+			for( ; turnNumber < 20; turnNumber++ )
+			{
+				if( maxScore == 1 )
+					break;
+	
+				startTimer( Player.Role.M );
+				Candidate c = getCandidateFromM( n );
+				pauseTimer( Player.Role.M );
+				if( c.isValid() )
+					confirmPlayer( Player.Role.M, "accepted Candidate" );
+				else {
+//					informPlayerOfError( Player.Role.M, c.getCachedMsg() );
+					throw new PlayerErrorException( getPlayer( Role.M ), c.getCachedMsg() );
+				}
+	
+	
+	
+				startTimer( Player.Role.P );
+				Noise noise = getNoiseFromP();
+				pauseTimer( Player.Role.P );
+				if( noise.isValid( p ) )
+					confirmPlayer( Player.Role.P, "accepted Noise" );
+				else {
+//					informPlayerOfError( Player.Role.P, noise.getCachedMsg() );
+					throw new PlayerErrorException( getPlayer(Role.P), noise.getCachedMsg() );
+				}
+	
+	
+	
+				double score = c.getScore( p, noise );              // noisy score
+				informPlayer( Player.Role.M, String.format("%.2f", score) );   // note that format() rounds the float
+				maxScore = Math.max( maxScore, c.getScore( p ) );   // actual score
+	
+			}
+	
+
+			finalScore = String.format("%.2f", maxScore);
+			turnsUsed = "" + turnNumber;
+		}
+		catch ( PlayerErrorException e )
 		{
-			if( maxScore == 1 )
-				break;
-
-			startTimer( Player.Role.M );
-			Candidate c = getCandidateFromM( n );
-			pauseTimer( Player.Role.M );
-			if( c.isValid() )
-				confirmPlayer( Player.Role.M, "accepted Candidate" );
-			else
-				informPlayerOfError( Player.Role.M, c.getCachedMsg() );
-
-
-
-			startTimer( Player.Role.P );
-			Noise noise = getNoiseFromP();
-			pauseTimer( Player.Role.P );
-			if( noise.isValid( p ) )
-				confirmPlayer( Player.Role.P, "accepted Noise" );
-			else
-				informPlayerOfError( Player.Role.P, noise.getCachedMsg() );
-
-
-
-			double score = c.getScore( p, noise );              // noisy score
-			informPlayer( Player.Role.M, String.format("%.2f", score) );   // note that format() rounds the float
-			maxScore = Math.max( maxScore, c.getScore( p ) );   // actual score
-
+			
+			if( e.getResponsiblePlayer() == getPlayer(Role.M) )
+			{
+				finalScore = "-1.0";
+				turnsUsed  = "20";
+			}
+			else  // if P is responsible
+			{
+				finalScore = "1.0";
+				turnsUsed  = "0";
+			}
+			
+			informPlayerOfError( getRoleOf(e.getResponsiblePlayer()), e.toString() );
+		}
+		finally
+		{
+			informPlayer( Player.Role.M, finalScore + " " + turnsUsed );
+			informPlayer( Player.Role.P, finalScore + " " + turnsUsed );
 		}
 
-
-
-		informPlayer( Player.Role.M, String.format("%.2f", maxScore) + " " + turnNumber );
-		informPlayer( Player.Role.P, String.format("%.2f", maxScore) + " " + turnNumber );
 	}
 
 	private static Noise getNoiseFromP() {
-//		Noise n = Noise.generateRandomNoise( p );
-//		System.out.println( "P->S: " + n );
-//		return n;
-		
-//		informPlayer( Role.P, "NOISE" );
-//		Message noise = io.receive( io.getPlayers().person );
-//		return new Noise( noise.toString() );
-		
 		Message response = io.requestForNoise(getPlayer(Player.Role.P));
 		return new Noise(response.toString());
 	}
 
 	private static Candidate getCandidateFromM(int n) {
-//		Candidate c = Candidate.generateRandomCandidate( n ); 
-//		System.out.println( "M->S: " + c );
-//		return c;
-		
-//		informPlayer( Role.M, "CANDIDATE" );
-//		Message candidate = io.receive( io.getPlayers().matchmaker );
-//		return new Candidate( candidate.toString() );
-		
 		Message response = io.requestForCandidate(getPlayer(Player.Role.M));
 		return new Candidate(response.toString());
 	}
@@ -191,19 +202,41 @@ public class Dating
 			return person;
 		}
 	}
+	
+	private static Role getRoleOf( Player player ) {
+		if( player == matchmaker )
+			return Role.M;
+		else
+			return Role.P;
+	}
 
 	private static Preferences getPlayerPreferences(int n)
 	{
-		// TODO Actually get from player
-//		return Preferences.generateRandomPreferences(n);
-		
-//		informPlayer( Role.P, "WEIGHTS" );
-//		Message prefs = io.receive( io.getPlayers().person );
-//		return new Preferences( prefs.toString() );
-		
 		Message response = io.requestForPlayerPreferences(getPlayer(Player.Role.P));
 		Preferences p = new Preferences(response.toString()); 
 		return p;
 	}
 
+	
+	
+	private static class PlayerErrorException extends Exception
+	{
+		private final Player responsiblePlayer;
+		
+		public PlayerErrorException( Player responsiblePlayer, String errorMessage )
+		{
+			super( errorMessage );
+			this.responsiblePlayer = responsiblePlayer;
+		}
+		
+		public Player getResponsiblePlayer()
+		{
+			return responsiblePlayer;
+		}
+		
+		public String toString()
+		{
+			return responsiblePlayer.toString() + ": " + super.toString();
+		}
+	}
 }
